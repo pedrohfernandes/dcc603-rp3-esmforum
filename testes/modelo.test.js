e037e3cf1bd3a06c92cadc242cc6afc30d2ffb43
Pedro Henrique <pedrohfernandes2001@gmail.com>
2025-06-21 16:46:02 -0300

    Criação de testes unitários para modelo.js

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -23,3 +23,43 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[2].num_respostas).toBe(0);
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
+
+test('Testando cadastro e get de respostas', () => {
+  //given
+  const id_pergunta = modelo.cadastrar_pergunta('1 + 1 = ?');
+  const id_resposta1 = modelo.cadastrar_resposta(id_pergunta, '1');
+  const id_resposta2 = modelo.cadastrar_resposta(id_pergunta, '2');
+  const id_resposta3 = modelo.cadastrar_resposta(id_pergunta, '3');
+
+  //when
+  const respostas = modelo.get_respostas(id_pergunta);
+
+  //then
+  expect(respostas.length).toBe(3);
+
+  expect(respostas[0].id_pergunta).toBe(id_pergunta);
+  expect(respostas[1].id_pergunta).toBe(id_pergunta);
+  expect(respostas[2].id_pergunta).toBe(id_pergunta);
+
+  expect(respostas[0].id_resposta).toBe(id_resposta1);
+  expect(respostas[0].texto).toBe('1');
+
+  expect(respostas[1].id_resposta).toBe(id_resposta2);
+  expect(respostas[1].texto).toBe('2');
+
+  expect(respostas[2].id_resposta).toBe(id_resposta3);
+  expect(respostas[2].texto).toBe('3');
+});
+
+test('Testando get de pergunta', () => {
+  //given
+  const id_pergunta = modelo.cadastrar_pergunta('1 + 1 = ?');
+  modelo.cadastrar_resposta(id_pergunta, '2');
+
+  //when
+  const pergunta = modelo.get_pergunta(id_pergunta);
+
+  //then
+  expect(pergunta.texto).toBe('1 + 1 = ?');
+  expect(pergunta.id_pergunta).toBe(id_pergunta);
+});
